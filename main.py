5f56c4904e530a7eb88de56b67fec91ce6363756
Dmitriy S. <fltask.pro@gmail.com>
2025-04-06 01:10:56 +0300

    add func question

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 def hello(name):
     print(f"Hello {name}")
+
+
+def question():
+    answer = input("How are you?\n")
+    return answer
+
+
+if __name__ == "__main__":
+    hello("Dima")
+    print(question())
